a232bd3dd81d2b0ca0ad3c9aa1fdc5dc8baf0a0e
mengeroshi <dpllgr@hotmail.com>
2021-10-28 00:11:19 -0500

    constructorsColors added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import {Barchart} from './charts/Barchart.jsx';
 
 function App() {
+  fetch('http://ergast.com/api/f1/2021/constructorStandings.json')
+  .then(response =>  {return response.json()})
+  .then(data => console.log(data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings));
   return (
     <div className="App">
       <Barchart/>
---
+++ src/utils/constructorsColors.js
@@ -0,0 +1,12 @@
+const constructorsColors = {
+    mercedes: '#00D2BE',
+    red_bull: '#16185F',
+    mclaren:'#FF8700',
+    ferrari: '#FF0000',
+    alpine: '#0E1C2C',
+    alphatauri: '#27284F',
+    aston_martin: '#02574F',
+    williams: '#02A8EA',
+    alfa: '#990000',
+    haas: '#ED1A3B',
+}
