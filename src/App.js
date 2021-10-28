3e614e7bd78159522906d707f4b3dc132bf47653
mengeroshi <dpllgr@hotmail.com>
2021-10-28 16:13:42 -0500

    data fethced from eargast API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-
+import React from 'react';
 import './App.css';
 import {Barchart} from './charts/Barchart.jsx';
 
+
+
 function App() {
-  fetch('http://ergast.com/api/f1/2021/constructorStandings.json')
-  .then(response =>  {return response.json()})
-  .then(data => console.log(data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings));
+  const [constructors, setConstructors] = React.useState([]); 
+
+  React.useEffect(() =>{
+    fetch('http://ergast.com/api/f1/2021/constructorStandings.json')
+    .then(response =>  {return response.json()})
+    .then(data =>{
+        const dataConstructors = data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
+        setConstructors(dataConstructors);
+        console.log(constructors);
+        
+    })
+    .catch( err => console.log(err));
+
+  }, [])
+  
   return (
     <div className="App">
-      <Barchart/>
+      <Barchart />
     </div>
   );
 }
