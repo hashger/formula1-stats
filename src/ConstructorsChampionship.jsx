8b0cb28e7d771b3dcbff48b3881a3ca255205818
mengeroshi <dpllgr@hotmail.com>
2021-10-28 23:55:56 -0500

    labels for bar chart added

--- src/ConstructorsChampionship.jsx
+++ src/ConstructorsChampionship.jsx
@@ -7,6 +7,7 @@ import {constructorsColors} from './utils/constructorsColors';
 
 function ConstructorsChampionship() {
   const [constructors, setConstructors] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
 
 
   React.useEffect(() =>{
@@ -26,21 +27,58 @@ function ConstructorsChampionship() {
           })
 
         setConstructors(constructorsArrayCurated);
+        setLoading(false)
           
       })
       .catch( err => console.log(err));
   
     }, [])
+
+    const options = {
+      indexAxis: 'y',
+      plugins:{
+        legend:{
+          display: true,
+          position: 'bottom',
+          
+        },
+        title:{
+          display: true,
+          text:'Formula 1 Constructors Championship 2021',
+          color: 'white',
+          font:{
+            size: 20,
+          }
+        }
+      },
+      elements:{
+        bar:{
+          inflateAmount: 0,
+        }
+      },
+      datasets:{
+        bar:{
+          barPercentage: 1,}
+      }
+      // scales:{
+      //   y:{
+      //     title:{display:true},
+      //     text:'obo'
+      //   }
+      // },
+    }
   
   return (
     <div className="App">
-      <Barchart 
+      {!loading && <Barchart 
         data = {constructors.map(constructor => constructor.points)}
         labels ={constructors.map(constructor => constructor.name)}
         colors = {constructors.map(constructor => constructor.color)}
         title={'Constructors Championship'}
+        options={options}
+        dataAlpha= {constructors}
 
-      />
+      /> }
     </div>
   );
 }
--- src/charts/Barchart.jsx
+++ src/charts/Barchart.jsx
@@ -5,27 +5,35 @@ import Chart from 'chart.js/auto';
 
 
 
-const Barchart = ({data, labels, colors, title}) =>{
+const Barchart = ({data, labels, colors, title, options, dataAlpha}) =>{
+
+    const obo = dataAlpha.map( constructor =>{
+        return{
+            label: constructor.name,
+            backgroundColor: constructor.color,
+            data: [constructor.points]
+        }
+    })
 
 
     const barChartOptions ={
         type: 'bar',
         data: {
-            labels: labels,
-            datasets: [{
-                label: title,
-                data: data,
-                backgroundColor: colors,
-            }]
+            labels: [''],
+            datasets: obo,
         },
+        options:options,
     }
 
 
     let canvasRef = React.useRef();
     React.useEffect(() =>{
-        
+        Chart.defaults.font.family='Formula1';
         const ctx = canvasRef.current.getContext('2d');
         let myChart = new Chart(ctx, barChartOptions);
+        //console.log(myChart.toBase64Image());
+        
+        
 
         return () => myChart.destroy();
 
